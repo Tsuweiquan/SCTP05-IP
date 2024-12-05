7f62e322ca16cae2b0a45a21a2dbd996425feb62
WeiQuan <weiquan@m-daq.com>
2024-12-05 21:42:27 +0800

    add in data prediction

--- flask_codebase/app.py
+++ flask_codebase/app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, redirect, url_for
 import logging
 import requests
 import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
 
 API_KEY = "da7cc643495745a78c99c491e1d4d0a6"
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -32,6 +34,7 @@ def data_transformation(data: dict) -> dict:
     df['high'] = df['high'].astype(float)
     df['low'] = df['low'].astype(float)
     df['close'] = df['close'].astype(float)
+    df['datetime'] = pd.to_datetime(df['datetime'], errors='coerce')
     logging.info(df)
     return df
     
@@ -55,6 +58,26 @@ def submit_search_historical_prices():
     
     # Display the data into html table
     price_table_html = df_data.to_html(classes='table table-bordered table-striped text-center', index=False, header=False)
+    
+    numeric_df = df_data.select_dtypes(include=['number'])
+    logging.info(numeric_df)
+    # Compute mean and covariance
+    df_mean = numeric_df.mean()
+    logging.info(df_mean)
+    cov_returns = numeric_df.cov()
+    logging.info(cov_returns)
+    
+    closedf = df_data[['datetime','close']]
+    print("Shape of close dataframe:", closedf.shape)
+    closedf = closedf[(closedf['datetime'] >= '2024-01-01') & (closedf['datetime'] <= '2024-07-26')]
+    close_stock = closedf.copy()
+    print("Total data for prediction: ",closedf.shape[0])
+    del closedf['datetime']
+    scaler=MinMaxScaler(feature_range=(0,1))
+    closedf=scaler.fit_transform(np.array(closedf).reshape(-1,1))
+    training_size=int(len(closedf)*0.70)
+    test_size=len(closedf)-training_size
+    train_data,test_data=closedf[0:training_size,:],closedf[training_size:len(closedf),:1]
     
     # return redirect(url_for('home'))
     return render_template('index.html', fig=None, price_table_html=price_table_html)
